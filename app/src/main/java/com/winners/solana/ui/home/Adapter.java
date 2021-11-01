6f7e23cff0cb09ef11d3da705ea6ce359339ba2a
abdulmirza <abdul_mirza805@hotmail.com>
2021-11-01 12:30:55 -0400

    Fixed some layout issues
    Fixed an issue where the wrong cell as being loaded into position 1.

--- app/src/main/java/com/winners/solana/ui/home/Adapter.java
+++ app/src/main/java/com/winners/solana/ui/home/Adapter.java
@@ -11,8 +11,9 @@ import com.winners.solana.R;
 import java.util.ArrayList;
 
 public class Adapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
-    private static final int TYPE_HEADER = 0;
-    private static final int TYPE_ITEM = 1;
+    private static final int TYPE_GRPAH = 0;
+    private static final int TYPE_TIME_BAR = 1;
+    private static final int TYPE_ITEM = 2;
 
     ArrayList<CellItem> cellItems;
     ArrayList<CardItem> cardItems;
@@ -28,10 +29,14 @@ public class Adapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
             View view = LayoutInflater.from(parent.getContext())
                     .inflate(R.layout.list_item_cell_info, parent, false);
             return new ViewHolderItem(view);
-        } else if (viewType == TYPE_HEADER) {
+        } else if (viewType == TYPE_GRPAH) {
             View view = LayoutInflater.from(parent.getContext())
                     .inflate(R.layout.list_item_graph, parent, false);
-            return new ViewHolderHeader(view);
+            return new ViewHolderGraph(view);
+        }else if (viewType == TYPE_TIME_BAR) {
+            View view = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.list_item_time_bar, parent, false);
+            return new ViewHolderTimeBar(view);
         }
 
         throw new RuntimeException("there is no type that matches the type " + viewType + " + make sure your using types correctly");
@@ -42,7 +47,9 @@ public class Adapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
         if (holder instanceof ViewHolderItem) {
             //String dataItem = getItem(position);
             //cast holder to VHItem and set data
-        } else if (holder instanceof ViewHolderHeader) {
+        } else if (holder instanceof ViewHolderGraph) {
+            //cast holder to VHHeader and set data for header.
+        } else if (holder instanceof ViewHolderTimeBar) {
             //cast holder to VHHeader and set data for header.
         }
     }
@@ -54,16 +61,22 @@ public class Adapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
     @Override
     public int getItemViewType(int position) {
-        if (position == 0 || position == 1)
-            return TYPE_HEADER;
+        if (position == 0)
+            return TYPE_GRPAH;
+        else if (position == 1)
+            return TYPE_TIME_BAR;
 
         return TYPE_ITEM;
     }
 
+    class ViewHolderGraph extends RecyclerView.ViewHolder {
+        public ViewHolderGraph(View itemView) {
+            super(itemView);
+        }
+    }
 
-
-    class ViewHolderHeader extends RecyclerView.ViewHolder {
-        public ViewHolderHeader(View itemView) {
+    class ViewHolderTimeBar extends RecyclerView.ViewHolder {
+        public ViewHolderTimeBar(View itemView) {
             super(itemView);
         }
     }
