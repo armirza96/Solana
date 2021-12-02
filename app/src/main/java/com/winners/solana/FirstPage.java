a75c38c5d4ccfec1c29bf0cdc78e795e7303bb86
Charlie Huang <74630670+CharlieHuang98@users.noreply.github.com>
2021-12-01 20:04:12 -0500

    I added stuff

--- app/src/main/java/com/winners/solana/FirstPage.java
+++ app/src/main/java/com/winners/solana/FirstPage.java
@@ -6,10 +6,20 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
 
 public class FirstPage extends AppCompatActivity {
 
-    protected Button button;
+    private Button Login;
+    private  Button Signup;
+    private EditText username;
+    private EditText password;
+
+    private FirebaseUser mUser;
+    private DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,11 +27,27 @@ public class FirstPage extends AppCompatActivity {
         setContentView(R.layout.activity_first_page);
         getSupportActionBar().hide();
         setTitle("Home Page");
-        button = findViewById(R.id.Goto);
-        button.setOnClickListener(new View.OnClickListener() {
+//        button = findViewById(R.id.Goto);
+        Login = findViewById(R.id.Login);
+        Signup = findViewById(R.id.Signup);
+        username = findViewById(R.id.Username);
+        password = findViewById(R.id.Password);
+        username.getText().toString().trim();
+        password.getText().toString().trim();
+
+        Login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(FirstPage.this,MainActivity.class));
+            }
+
+        });
+
+        Signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(FirstPage.this,SignupActivity.class));
+
             }
         });
     }
---
+++ app/src/main/java/com/winners/solana/User.java
@@ -0,0 +1,20 @@
+package com.winners.solana;
+
+public class User {
+    public String Username;
+
+    public User(String username) {
+        Username = username;
+    }
+
+    public User() {
+    }
+
+    public String getUsername() {
+        return Username;
+    }
+
+    public void setUsername(String username) {
+        Username = username;
+    }
+}
